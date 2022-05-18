8e5101171c4bdaa40cc1b6c50df0289f383c596f
Jesse Abdul <jesse.abdul@noaa.gov>
2022-05-17 14:48:46 -1000

    updated DB/app

--- GIM/docs/PIFSC Resource Inventory Git Info Module - Technical Documentation.md
+++ GIM/docs/PIFSC Resource Inventory Git Info Module - Technical Documentation.md
@@ -8,9 +8,11 @@ The PIFSC Resource Inventory (PRI) Git Info Module (GIM) was developed to retrie
     -   URL:git@picgitlab.nmfs.local:centralized-data-tools/pifsc-resource-inventory.git in the [GIM](../) folder
     -   Application: 0.3 (Git tag: pifsc_resource_inventory_git_info_app_v0.3)
 -   [PRI Database Documentation](../../docs/PIFSC%20Resource%20Inventory%20Database%20Documentation.md)
+-   [Business Rule Documentation](../../docs/PIFSC%20Resource%20Inventory%20-%20Business%20Rule%20Documentation.md)
+    -   [Business Rule List](../../docs/PRI%20Business%20Rule%20List.xlsx)
 
 ## Requirements:
--   PHP 5.x
+-   PHP 5.x or higher (tested with PHP5.5 and PHP8)
 -   Oracle 12c or higher
 
 ## Data Flow:
@@ -36,15 +38,21 @@ The PIFSC Resource Inventory (PRI) Git Info Module (GIM) was developed to retrie
     -   GITLAB_HOST_NAME can remain unchanged
     -   GITLAB_API_KEY is the API key for the PIFSC GitLab server
     -   PROJ_SOURCE can remain unchanged
+-   Update the [db_connection_info.php](../application_code/functions/db_connection_info.php) configuration file to specify the hostname/username/password for the application schema (PRI_RIA_APP) used for database interactions
 
 ## Database Setup:
 -   [Installing or Upgrading the Database](../../docs/PIFSC%20Resource%20Inventory%20-%20Installing%20or%20Upgrading%20the%20Database.md)
+
+## Business Rules:
+-   The business rules for the GIM are defined in the [Business Rule Documentation](../../docs/PIFSC%20Resource%20Inventory%20-%20Business%20Rule%20Documentation.md) and each specific business rule listed in the [Business Rule List](../../docs/PRI%20Business%20Rule%20List.xlsx) with a Scope of "GIM" apply to this module and each rule with a Scope of "PRI Database" apply to the underlying database
 
 ## Features:
 -   SQL Transactions are implemented when importing the project, tag, etc. information from the PIFSC GitLab server.  If there are errors encountered during the processing of a given project the transaction is rolled back.  The script logs will need to be consulted to determine the conditions that led up to the given project SQL processing error.  
 -   Script Logs
     -   The database script log can be accessed via the DB_LOG_ENTRIES_V view for entries with LOG_SOURCE = 'retrieve_gitlab_info.php'
     -   The file-based script log can be accessed in the working copy of the PRI repository in the [logs](../application_code/logs) folder (retrieve_gitlab_info_[YYYYMMDD]_[HH24_MI].log based on the date/time the script was executed)
+-   Custom Configuration Files
+    -   Project resources can be defined using the [Project Resource Definition SOP](./Project%20Resource%20Definition%20SOP.md)
 
 ## Application Security:
 -   [Principle of least privilege](https://docs.google.com/document/d/15qW2pDHM8bebmNJ76AfC-SgICKQPGmKSiUkXbrZ7OVQ/edit?usp=sharing): All of the data tables and support objects are defined in the PRI data schema (PRI), the GIM's parsing schema (shadow schema) which is used to actually interact with the underlying database is PRI_GIM_APP. PRI_GIM_APP has very limited permissions on the PRI schema based on the required functionality of the application (see [PRI_GIM_APP_permissions.xlsx](./PRI_GIM_APP_permissions.xlsx)) to implement the principle of least privilege. The PRI_GIM_APP schema has not been granted any roles in the database instance.
--- shared_SQL/upgrades/pifsc_resource_inventory_DDL_DML_upgrade_v0.3.sql
+++ shared_SQL/upgrades/pifsc_resource_inventory_DDL_DML_upgrade_v0.3.sql
@@ -25,6 +25,9 @@ ADD (RES_DEMO_URL VARCHAR2(2000) );
 
 COMMENT ON COLUMN PRI_PROJ_RES.RES_DEMO_URL IS 'The live demonstration URL for the project resource';
 
+ALTER TABLE PRI_PROJ_RES
+MODIFY (RES_COLOR_CODE NULL);
+
 
 CREATE OR REPLACE VIEW
 PRI_RES_V
@@ -163,140 +166,6 @@ COMMENT ON COLUMN PRI_PROJ_V.RES_DEMO_URL IS 'The live demonstration URL for the
 
 
 
---Detailed query to show all associations of each resource and the highest version of each module (need to join it to the max version information twice but I think it should be able to filter on both times based on the PROJ_ID values matching or not.) - the view should be queried once and the result sets reused if the DB engine works properly
-CREATE OR REPLACE VIEW PRI_RES_PROJ_TAG_MAX_V
-
-AS
-SELECT
-PRI_PROJ_V.PROJ_ID,
-PRI_PROJ_V.VC_PROJ_ID,
-PRI_PROJ_V.PROJ_NAME,
-PRI_PROJ_V.PROJ_DESC,
-PRI_PROJ_V.SSH_URL,
-PRI_PROJ_V.HTTP_URL,
-PRI_PROJ_V.README_URL,
-PRI_PROJ_V.AVATAR_URL,
-PRI_PROJ_V.PROJ_CREATE_DTM,
-PRI_PROJ_V.PROJ_UPDATE_DTM,
-PRI_PROJ_V.PROJ_VISIBILITY,
-PRI_PROJ_V.PROJ_NAME_SPACE,
-PRI_PROJ_V.PROJ_SOURCE,
-PRI_PROJ_V.RES_ID,
-PRI_PROJ_V.RES_CATEGORY,
-PRI_PROJ_V.RES_TAG_CONV,
-PRI_PROJ_V.RES_NAME,
-PRI_PROJ_V.RES_COLOR_CODE,
-PRI_PROJ_V.RES_URL,
-PRI_PROJ_V.RES_SCOPE_ID,
-PRI_PROJ_V.RES_SCOPE_CODE,
-PRI_PROJ_V.RES_SCOPE_NAME,
-PRI_PROJ_V.RES_SCOPE_DESC,
-PRI_PROJ_V.RES_TYPE_ID,
-PRI_PROJ_V.RES_TYPE_CODE,
-PRI_PROJ_V.RES_TYPE_NAME,
-PRI_PROJ_V.RES_TYPE_DESC,
-PRI_PROJ_V.RES_DESC,
-PRI_PROJ_V.RES_DEMO_URL,
-RES_PROJ_VERS.MAX_TAG_ID RES_MAX_TAG_ID,
-RES_PROJ_VERS.MAX_VERS_NUM RES_MAX_VERS_NUM,
-TAG_PROJ_VERS.MAX_TAG_ID TAG_PROJ_MAX_TAG_ID,
-TAG_PROJ_VERS.MAX_VERS_NUM TAG_PROJ_MAX_VERS_NUM,
-(CASE WHEN RES_PROJ_VERS.MAX_VERS_NUM = TAG_PROJ_VERS.MAX_VERS_NUM THEN 'Y' WHEN RES_PROJ_VERS.MAX_VERS_NUM IS NULL OR TAG_PROJ_VERS.MAX_VERS_NUM IS NULL THEN NULL ELSE 'N' END) TAG_MAX_VERS_YN,
-(CASE WHEN RES_PROJ_VERS.MAX_VERS_NUM = TAG_PROJ_VERS.MAX_VERS_NUM THEN 'Current Version' WHEN RES_PROJ_VERS.MAX_VERS_NUM IS NULL OR TAG_PROJ_VERS.MAX_VERS_NUM IS NULL THEN NULL ELSE 'Update Available' END) TAG_VERS_STATUS,
-TAG_PROJ.PROJ_ID TAG_PROJ_ID,
-TAG_PROJ.VC_PROJ_ID TAG_VC_PROJ_ID,
-TAG_PROJ.PROJ_NAME TAG_PROJ_NAME,
-TAG_PROJ.PROJ_DESC TAG_PROJ_DESC,
-TAG_PROJ.SSH_URL TAG_SSH_URL,
-TAG_PROJ.HTTP_URL TAG_HTTP_URL,
-TAG_PROJ.README_URL TAG_README_URL,
-TAG_PROJ.AVATAR_URL TAG_AVATAR_URL,
-TAG_PROJ.PROJ_CREATE_DTM TAG_PROJ_CREATE_DTM,
-TAG_PROJ.PROJ_UPDATE_DTM TAG_PROJ_UPDATE_DTM,
-TAG_PROJ.PROJ_VISIBILITY TAG_PROJ_VISIBILITY,
-TAG_PROJ.PROJ_NAME_SPACE TAG_PROJ_NAME_SPACE,
-TAG_PROJ.PROJ_SOURCE TAG_PROJ_SOURCE
-
-FROM
-
---get the projects associated with each of the resources
-PRI_PROJ_V
---join the resources to the maximum version information for the corresponding resource (res and tag projects are equivalent so this is the maximum version number current defined for the project resource) - show only the projects with resources
-LEFT JOIN PRI_RES_TAG_MAX_VERS_V RES_PROJ_VERS
-ON PRI_PROJ_V.RES_ID = RES_PROJ_VERS.RES_ID
-AND RES_PROJ_VERS.RES_PROJ_ID = RES_PROJ_VERS.TAG_PROJ_ID
-
---join the tagged projects to the maximum version numbers of the associated resources (exclude the projects that are the same since those don't need to be included in the list)
-LEFT JOIN PRI_RES_TAG_MAX_VERS_V TAG_PROJ_VERS
-ON PRI_PROJ_V.RES_ID = TAG_PROJ_VERS.RES_ID
-AND TAG_PROJ_VERS.RES_PROJ_ID <> TAG_PROJ_VERS.TAG_PROJ_ID
-
---join all the projects that tag the given resource
-LEFT JOIN PRI_PROJ TAG_PROJ
-ON TAG_PROJ.PROJ_ID = TAG_PROJ_VERS.TAG_PROJ_ID
-
-WHERE PRI_PROJ_V.RES_ID IS NOT NULL
-
-ORDER BY PRI_PROJ_V.PROJ_NAME,
-PRI_PROJ_V.RES_NAME,
-TAG_PROJ.PROJ_NAME
-;
-COMMENT ON TABLE PRI_RES_PROJ_TAG_MAX_V IS 'Resource and Implemented Project Maximum Versions (View)
-
-This view returns all project resources (including highest version) and all associated projects that tag the resource (as implemented) including the highest version number for the associated tagged project.';
-
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_ID IS 'Primary key for the PRI_PROJ table';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.VC_PROJ_ID IS 'Unique numeric ID of the project in the given version control system';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_NAME IS 'Name of the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_DESC IS 'Description of the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.SSH_URL IS 'SSH URL for the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.HTTP_URL IS 'HTTP URL for the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.README_URL IS 'Readme URL for the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.AVATAR_URL IS 'Avatar URL for the project';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_CREATE_DTM IS 'The date/time the project was created';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_UPDATE_DTM IS 'The date/time the project was last updated';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_VISIBILITY IS 'The visibility for the project (public, internal, private)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_NAME_SPACE IS 'project name including the namespace prefix';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.PROJ_SOURCE IS 'the source of the project record (e.g. PIFSC GitLab, GitHub, manual entry)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_ID IS 'Primary key for the PRI_PROJ_RES table';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_CATEGORY IS 'The resource category (free form text) - examples values include Development Tool, Data Management Tool, Centralized Database Applications';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_TAG_CONV IS 'Tag Naming convention used to identify the given project resource''s version.  The suffix is required to be a series of period-delimited numbers (e.g. for a naming convention of db_module_packager_v the tag value of db_module_packager_v1.13.4 is valid)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_NAME IS 'The name of the project resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_COLOR_CODE IS 'The color code for the project resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_URL IS 'The URL for the project resource (this is blank when the repository URL is the same as the resource URL)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_SCOPE_ID IS 'Foreign key reference to the resource scope';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_SCOPE_CODE IS 'Code for the given Resource Scope';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_SCOPE_NAME IS 'Name of the given Resource Scope';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_SCOPE_DESC IS 'Description for the given Resource Scope';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_TYPE_ID IS 'Foreign key reference to the resource type';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_TYPE_CODE IS 'Code for the given Resource Type';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_TYPE_NAME IS 'Name of the given Resource Type';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_TYPE_DESC IS 'Description for the given Resource Type';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_MAX_TAG_ID IS 'The TAG_ID value associated with the highest version number of the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_MAX_VERS_NUM IS 'The parsed version number for the maximum installed version of the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_MAX_TAG_ID IS 'The TAG_ID value associated with the highest version number of the project associated with the given resource (identified by TAG_PROJ_ID)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_MAX_VERS_NUM IS 'The parsed version number associated with the highest version number of the project associated with the given resource (identified by TAG_PROJ_ID)';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_MAX_VERS_YN IS 'Flag to indicate if the resource''s maximum version is the same as the maximum version implemented on the associated project (Y) or not (N).  This is used to identify implementations that are out of date';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_VERS_STATUS IS 'Flag to indicate if the resource''s maximum version is the same as the maximum version implemented on the associated project (Current Version) or not (Update Available).  This is used to identify implementations that are out of date';
-
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_ID IS 'Primary key for the PRI_PROJ table for the project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_VC_PROJ_ID IS 'Unique numeric ID of the project in the given version control system for the project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_NAME IS 'Name of the project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_DESC IS 'Description of the project project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_SSH_URL IS 'SSH URL for the project project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_HTTP_URL IS 'HTTP URL for the project project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_README_URL IS 'Readme URL for the project project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_AVATAR_URL IS 'Avatar URL for the project project associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_CREATE_DTM IS 'The date/time the project associated with the given resource was created';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_UPDATE_DTM IS 'The date/time the project associated with the given resource was last updated';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_VISIBILITY IS 'The visibility for the project (public, internal, private) associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_NAME_SPACE IS 'project name including the namespace prefix associated with the given resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_SOURCE IS 'the source of the project record (e.g. PIFSC GitLab, GitHub, manual entry) associated with the given resource';
-
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_DESC IS 'The description for the project resource';
-COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_DEMO_URL IS 'The live demonstration URL for the project resource';
-
-ALTER VIEW PRI_RES_PROJ_TAG_MAX_SUM_V COMPILE;
 
 
 
@@ -638,6 +507,7 @@ PRI_PROJ_V.RES_TYPE_ID,
 PRI_PROJ_V.RES_TYPE_CODE,
 PRI_PROJ_V.RES_TYPE_NAME,
 PRI_PROJ_V.RES_TYPE_DESC,
+PRI_PROJ_V.RES_DESC,
 PRI_PROJ_V.RES_DEMO_URL,
 RES_PROJ_VERS.MAX_TAG_ID RES_MAX_TAG_ID,
 RES_PROJ_VERS.MAX_VERS_NUM RES_MAX_VERS_NUM,
@@ -737,6 +607,8 @@ COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.TAG_PROJ_SOURCE IS 'the source of the p
 
 
 COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_DEMO_URL IS 'The live demonstration URL for the project resource';
+
+COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_V.RES_DESC IS 'The description for the project resource';
 
 
 CREATE OR REPLACE VIEW
@@ -889,7 +761,7 @@ COMMENT ON COLUMN PRI_RES_PROJ_TAG_MAX_SUM_ALL_V.RES_DEMO_URL IS 'The live demon
 
 
 --define the upgrade version in the database upgrade log table:
-INSERT INTO DB_UPGRADE_LOGS (UPGRADE_APP_NAME, UPGRADE_VERSION, UPGRADE_DATE, UPGRADE_DESC) VALUES ('PIFSC Resource Inventory', '0.3', TO_DATE('11-MAY-22', 'DD-MON-YY'), '');
+INSERT INTO DB_UPGRADE_LOGS (UPGRADE_APP_NAME, UPGRADE_VERSION, UPGRADE_DATE, UPGRADE_DESC) VALUES ('PIFSC Resource Inventory', '0.3', TO_DATE('17-MAY-22', 'DD-MON-YY'), 'Updated PRI_PROJ_RES to add the RES_DESC and RES_DEMO_URL fields; updated the RES_URL to be a longer string data type and allowed.  Updated PRI_RES_V, PRI_PROJ_V, and PRI_RES_PROJ_TAG_MAX_V to add the new PRI_PROJ_RES fields.  Updated PRI_PROJ_RES_TAG_MAX_SUM_V to add in BR-delimited and BR-delimited formatted link fields.  Created PRI_PROJ_RES_TAG_MAX_SUM_ALL_V to add the detailed project information for each project.  Updated PRI_RES_PROJ_TAG_MAX_SUM_V to add in BR-delimited and BR-delimited formatted link fields for the associated projects of each resource.  Created PRI_RES_PROJ_TAG_MAX_SUM_ALL_V to add the detailed resource information for each resource.');
 
 --commit the DB_UPGRADE_LOGS record insertion
 COMMIT;
