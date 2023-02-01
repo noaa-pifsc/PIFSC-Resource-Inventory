1dbec4fbb18066326b6fbeebcc0369bcd8dacf7e
Jesse Abdul <jesse.abdul@noaa.gov>
2023-01-31 17:32:53 -1000

    updated summary report page and styling

--- RIA/application_code/index.php
+++ RIA/application_code/index.php
@@ -49,7 +49,7 @@
 			//the database connection was successful, continue generating the initial page content:
 
 
-      //define the SQL for the query to retrieve the specified resource:
+      //define the SQL for the query to retrieve the project report:
       $SQL = "select PROJ_VISIBILITY, count(*) NUM_PROJECTS from pri_proj where PROJ_VISIBILITY <> 'private' group by PROJ_VISIBILITY order by PROJ_VISIBILITY";
 
       //initialize $dummy_id variable for the query:
@@ -62,28 +62,69 @@
 
 				$table_buffer = thead_tag(tr_tag(th_tag("Visibility", array("class=\"tooltip\"", "title=\"Project visibility\"")).th_tag("# Projects", array("class=\"tooltip\"", "title=\"Number of projects\""))));
 
+				//string buffer variable for generating the table rows that will be enclosed by a tbody tag
+				$row_buffer = '';
+
         //loop through result set:
         while ($row = $oracle_db->fetch($stid))
         {
           //the resource exists, generate the page content and display it:
 
-          $table_buffer .= tr_tag(td_tag($row['PROJ_VISIBILITY']).td_tag($row['NUM_PROJECTS']));
+          $row_buffer .= tr_tag(td_tag($row['PROJ_VISIBILITY'], array("class=\"text_cell\"")).td_tag($row['NUM_PROJECTS'], array("class=\"number_cell\"")));
 
         }
-				$string_buffer .= div_tag(table_tag($table_buffer, array("class=\"report_table\"")), array("class=\"report_table_container\""));
+				$string_buffer .= div_tag("Projects", array("class=\"table_heading\"")).div_tag(table_tag($table_buffer.tbody_tag($row_buffer), array("class=\"report_table\"")), array("class=\"report_table_container\""));
 
 				unset ($table_buffer);
 
       }
       else
       {
-        //the resource information could not be queried successfully:
-        $string_buffer .= b_tag("There was a problem with the database and your request could not be processed, please try again later");
+				//the resource information could not be queried successfully:
+				$string_buffer .= b_tag("The projects could not be retrieved from the database");
 
 //        $resource->add_message("The query failed");
 
       }
 
+
+			//define the SQL for the query to retrieve the resource report:
+			$SQL = "select RES_SCOPE_NAME, RES_CATEGORY, count(*) NUM_RESOURCES from PRI_PROJ_V where PROJ_VISIBILITY <> 'private' AND RES_ID IS NOT NULL group by RES_CATEGORY, RES_SCOPE_NAME order by RES_SCOPE_NAME, RES_CATEGORY";
+
+			//initialize $dummy_id variable for the query:
+			$dummy_id = null;
+
+			//execue the SQL query:
+			if ($oracle_db->query($SQL, $stid, $dummy_id, array()))
+			{
+				//the query was successful:
+
+				$table_buffer = thead_tag(tr_tag(th_tag("Scope", array("class=\"tooltip\"", "title=\"The scope for the given resource (e.g. Software Development, Data Stewardship)\"")).th_tag("Category", array("class=\"tooltip\"", "title=\"The category for the given resource (e.g. Software Tool, Training Materials)\"")).th_tag("# Resources", array("class=\"tooltip\"", "title=\"Number of Resources\""))));
+
+				//string buffer variable for generating the table rows that will be enclosed by a tbody tag
+				$row_buffer = '';
+
+				//loop through result set:
+				while ($row = $oracle_db->fetch($stid))
+				{
+					//the resource exists, generate the page content and display it:
+
+					$row_buffer .= tr_tag(td_tag($row['RES_SCOPE_NAME'], array("class=\"text_cell\"")).td_tag($row['RES_CATEGORY'], array("class=\"text_cell\"")).td_tag($row['NUM_RESOURCES'], array("class=\"number_cell\"")));
+
+				}
+				$string_buffer .= div_tag("Resources", array("class=\"table_heading\"")).div_tag(table_tag($table_buffer.tbody_tag($row_buffer), array("class=\"report_table\"")), array("class=\"report_table_container\""));
+
+				unset ($table_buffer);
+
+			}
+			else
+			{
+				//the resource information could not be queried successfully:
+				$string_buffer .= b_tag("The resources could not be retrieved from the database");
+
+			//        $resource->add_message("The query failed");
+
+			}
 
 		}
 
