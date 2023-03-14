f341815f05a68aa693e38127e3bd2317c64eab7b
Jesse Abdul <jesse.abdul@noaa.gov>
2023-03-13 17:41:05 -1000

    updated GIM script and crontab definition

--- docker/config/run_GIM.sh
+++ docker/config/run_GIM.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
 # echo "load the oracle environment variables"
-source /usr/src/ODS/config/oracle_env
+source /usr/src/PRI/config/oracle_env
 # execute the ODS generator script
 /opt/remi/php81/root/usr/bin/php /usr/src/GIM/retrieve_gitlab_info.php
