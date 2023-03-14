5e6ea88f29aa25fc34f143ab17f063f8a1f9e126
Jesse Abdul <jesse.abdul@noaa.gov>
2023-03-13 17:46:51 -1000

    fixed path issue

--- docker/config/run_GIM.sh
+++ docker/config/run_GIM.sh
@@ -2,4 +2,4 @@
 # echo "load the oracle environment variables"
 source /usr/src/PRI/config/oracle_env
 # execute the ODS generator script
-/opt/remi/php81/root/usr/bin/php /usr/src/GIM/retrieve_gitlab_info.php
+/opt/remi/php81/root/usr/bin/php /usr/src/PRI/GIM/retrieve_gitlab_info.php
