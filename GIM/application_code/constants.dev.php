e1fc0d3de13b373ac4e635291f7477e7bcc8ab4d
Jesse Abdul <jesse.abdul@noaa.gov>
2023-03-14 13:17:39 -1000

    removed logging statements from PRI_gitlab.php and used the logging folder constant value

--- GIM/application_code/constants.dev.php
+++ GIM/application_code/constants.dev.php
@@ -7,6 +7,10 @@
 
     //define the hostname for the destination gitlab server
     define ("GITLAB_HOST_NAME", "picgitlab.nmfs.local");
+	
+	//logging directory
+	define ("LOG_FILE_DIR", "/usr/src/PRI/logs/");
+
     //define the API key for an administrator account on the source gitlab server to allow the Gitlab instance to be queried while having all projects visible:
     define ("GITLAB_API_KEY", "");
 
